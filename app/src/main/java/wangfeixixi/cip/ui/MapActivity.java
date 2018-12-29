30d82b414b40c8200f62a94eeaedded025bc3a0e
xuanyuanxixi <1020301980@qq.com>
2018-12-29 16:04:14 +0800

    初步界面元素ok

--- app/src/main/java/wangfeixixi/cip/ui/MapActivity.java
+++ app/src/main/java/wangfeixixi/cip/ui/MapActivity.java
@@ -1,28 +1,26 @@
 package wangfeixixi.cip.ui;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.amap.api.navi.model.AMapNaviTrafficFacilityInfo;
 import com.amap.api.navi.model.AimLessModeCongestionInfo;
 import com.amap.api.navi.model.AimLessModeStat;
+import com.bumptech.glide.Glide;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import java.text.DecimalFormat;
 
+import wangfeixixi.cip.R;
 import wangfeixixi.cip.beans.JsonRootBean;
 import wangfeixixi.cip.fram.BaseActivity;
 import wangfeixixi.cip.widget.carview.CarUtils;
-import wangfeixixi.cip.widget.carview.child.ChildContainer;
 import wangfeixixi.cip.widget.map.LBSUtils;
 import wangfeixixi.com.base.ScreenUtils;
 import wangfeixixi.com.base.VertionUtils;
@@ -32,101 +30,119 @@ import wangfeixixi.com.bdvoice.VoiceUtil;
 import wangfeixixi.lbs.gaode.GaodeMapService;
 
 public class MapActivity extends BaseActivity {
-    private RelativeLayout rl_father;
     private FrameLayout mapContainer;
     private GaodeMapService mLbs;
-    private RelativeLayout rl_capion;
+    private LinearLayout ll_capion;
     private TextView tv_log;
+    private TextView tv_speed;
+    private TextView tv_distance;
+    private ImageView iv_left;
+    private ImageView iv_up;
+    private ImageView iv_down;
+    private ImageView iv_right;
+
 
     @Override
     protected void initView(Bundle savedInstanceState) {
-        //添加总布局
-        rl_father = new RelativeLayout(this);
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        layoutParams.width = (int) (ScreenUtils.getScreenWidth());
-        layoutParams.height = (int) (ScreenUtils.getScreenHeight());
-        getWindow().addContentView(rl_father, layoutParams);
-        //添加地图
-        mapContainer = ChildContainer.addAllMap(rl_father);
+        setContentView(R.layout.map_activity);
+
+        mapContainer = findViewById(R.id.fl_map_container);
         mLbs = new GaodeMapService(this);
         mapContainer.addView(mLbs.getMap());
         mLbs.onCreate(savedInstanceState);
 
-        //添加半屏提示
-        rl_capion = new RelativeLayout(this);
-        RelativeLayout.LayoutParams rllp = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        rllp.width = (int) (ScreenUtils.getScreenWidth());
-        rllp.height = (int) (ScreenUtils.getScreenHeight() / 3);
-        rl_capion.setBackgroundColor(Color.parseColor("#dddddd"));
-        rl_father.addView(rl_capion, rllp);
-        switchCapionHeight(0);
-        addLogView();
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_distance = findViewById(R.id.tv_distance);
+
+        ll_capion = findViewById(R.id.ll_capion);
+
+        iv_left = findViewById(R.id.iv_left);
+        iv_up = findViewById(R.id.iv_up);
+        iv_down = findViewById(R.id.iv_down);
+        iv_right = findViewById(R.id.iv_right);
+
+//        addLogView();
     }
 
-    private void addLogView() {
-        //日志界面
-        tv_log = new TextView(this);
-        tv_log.setVisibility(View.GONE);
-        rl_father.addView(tv_log);
-        //日志按钮
-        Button btn_log = new Button(this);
-        RelativeLayout.LayoutParams btn_rllp = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        btn_rllp.topMargin = ScreenUtils.getScreenHeight() - 280;
-        btn_rllp.leftMargin = ScreenUtils.getScreenWidth() - 280;
-        btn_log.setText("点击/长按");
-        btn_log.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switchLogView();
-            }
-        });
-        btn_log.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                startActivity(new Intent(mCtx, NewMapActivity.class));
-                return false;
-            }
-        });
-        rl_father.addView(btn_log, btn_rllp);
-
-    }
+//    private void addLogView() {
+//        //日志界面
+//        tv_log = new TextView(this);
+//        tv_log.setVisibility(View.GONE);
+//        rl_father.addView(tv_log);
+//        //日志按钮
+//        Button btn_log = new Button(this);
+//        RelativeLayout.LayoutParams btn_rllp = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//        btn_rllp.topMargin = ScreenUtils.getScreenHeight() - 280;
+//        btn_rllp.leftMargin = ScreenUtils.getScreenWidth() - 280;
+//        btn_log.setText("点击/长按");
+//        btn_log.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                switchLogView();
+//            }
+//        });
+//        btn_log.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//                startActivity(new Intent(mCtx, NewMapActivity.class));
+//                return false;
+//            }
+//        });
+//        rl_father.addView(btn_log, btn_rllp);
+//
+//    }
 
     private void switchLogView() {
         tv_log.setVisibility(tv_log.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
-//        //使用Glide框架加载图片
-//        Glide.with(imageView.getContext())
-//                .load(url)
-//                .apply(new RequestOptions().placeholder(placeholderRes))
-//                .into(imageView);
+
     }
 
+    private void switchCW(int cw) {
+        switch (cw) {
+            case 0://预警消除
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void loadImageView(ImageView imageView) {
+        Glide.with(imageView.getContext())
+                .load(R.mipmap.cw_back)
+                .into(imageView);
+    }
 
     public void switchCapionHeight(int height) {
-        RelativeLayout.LayoutParams map_rllp = (RelativeLayout.LayoutParams) mapContainer.getLayoutParams();
-        RelativeLayout.LayoutParams capion_rllp = (RelativeLayout.LayoutParams) rl_capion.getLayoutParams();
         if (height == 0) {
             mLbs.setPointToCenter(ScreenUtils.getScreenWidth() / 2, ScreenUtils.getScreenHeight() / 2);
-            map_rllp.width = ScreenUtils.getScreenWidth();
-            map_rllp.height = ScreenUtils.getScreenHeight();
-            map_rllp.topMargin = 0;
-
-            capion_rllp.width = ScreenUtils.getScreenWidth();
-            capion_rllp.height = 10;
+            ll_capion.setVisibility(View.GONE);
         } else {
-            map_rllp.width = ScreenUtils.getScreenWidth();
-            map_rllp.height = ScreenUtils.getScreenHeight() / 3;
-            map_rllp.topMargin = ScreenUtils.getScreenHeight() / 3 * 2;
             mLbs.setPointToCenter(ScreenUtils.getScreenWidth() / 2, ScreenUtils.getScreenHeight() / 6);
-
-            capion_rllp.width = ScreenUtils.getScreenWidth();
-            capion_rllp.height = ScreenUtils.getScreenHeight() / 3 * 2;
+            ll_capion.setVisibility(View.VISIBLE);
         }
-        rl_capion.setLayoutParams(capion_rllp);
-        mapContainer.setLayoutParams(map_rllp);
     }
 
     @Override
     protected void initData() {
+        Glide.with(mCtx)
+                .load(R.mipmap.cw_front)
+                .into(iv_up);
+        Glide.with(mCtx)
+                .load(R.mipmap.cw_back)
+                .into(iv_down);
+        Glide.with(mCtx)
+                .load(R.mipmap.cw_left)
+                .into(iv_left);
+
+        Glide.with(mCtx)
+                .load(R.mipmap.cw_right)
+                .into(iv_right);
     }
 
     public long lastTime = 0;
@@ -134,23 +150,27 @@ public class MapActivity extends BaseActivity {
     @Override
     protected void onReceiveJsonBean(JsonRootBean bean) {
         //显示log
-        addLog(bean);
+//        addLog(bean);
         if (bean.hvDatas == null || bean.rvDatas == null) {
             return;
         }
+        tv_speed.setText(String.valueOf(Double.parseDouble(new DecimalFormat("#.#").format(bean.hvDatas.speed * 3.6f))));
+        tv_distance.setText(String.valueOf(Double.parseDouble(new DecimalFormat("#.#").format(bean.rvDatas.get(0).distance))));
 
         long nowTime = System.currentTimeMillis();
         //更新地图位置
         LBSUtils.addBenMarker(mLbs, bean.hvDatas);
         for (int i = 0; i < bean.rvDatas.size(); i++)
             LBSUtils.addOtherMarker(mLbs, bean.rvDatas.get(i));
+
+        switchCapionHeight(1);
         if (nowTime - lastTime > 2000) {
             //语音提示
-            if (bean.rvDatas.get(0) != null && bean.rvDatas.get(0).fcwAlarm != 0) {
-                switchCapionHeight(1);
-            } else {
-                switchCapionHeight(0);
-            }
+//            if (bean.rvDatas.get(0) != null && bean.rvDatas.get(0).fcwAlarm != 0) {
+//                switchCapionHeight(1);
+//            } else {
+//                switchCapionHeight(0);
+//            }
             VoiceUtil.getInstance().speek("保持距离");
             lastTime = nowTime;
         }
