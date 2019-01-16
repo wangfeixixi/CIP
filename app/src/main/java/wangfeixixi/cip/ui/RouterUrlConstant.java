35cf553e85a7bd28966640a83263f18681e8f13d
xuanyuanxixi <1020301980@qq.com>
2019-01-16 15:51:51 +0800

    组件化测试

--- app/src/main/java/wangfeixixi/cip/ui/RouterUrlConstant.java
+++
@@ -1,7 +0,0 @@
-package wangfeixixi.cip.ui;
-
-public class RouterUrlConstant {
-    public static final String MAP = "/map/MapActivity";
-    public static final String BIRD = "/bird/BirdActivity";
-
-}
